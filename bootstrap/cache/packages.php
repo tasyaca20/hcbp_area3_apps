924909b80fc3cfea87e6ec6277bd84befe262320
tasyaca20 <tasyaaaputriii09@gmail.com>
2026-08-31 00:24:00 -0700

    fix: provide immutable Laravel package discovery manifest

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -1,1 +1,10 @@
-<?php return [];
+<?php return [
+    'maatwebsite/excel' => [
+        'providers' => [
+            'Maatwebsite\\Excel\\ExcelServiceProvider',
+        ],
+        'aliases' => [
+            'Excel' => 'Maatwebsite\\Excel\\Facades\\Excel',
+        ],
+    ],
+];
