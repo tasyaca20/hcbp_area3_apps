0de8583b7b8bbb27904c6a495be3e15104095421
tasyaca20 <tasyaaaputriii09@gmail.com>
2026-08-30 23:54:23 -0700

    fix: use standard Laravel application bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,16 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-use Illuminate\View\ViewServiceProvider;
-use Maatwebsite\Excel\ExcelServiceProvider;
-use App\Providers\AppServiceProvider;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withProviders([
-        ViewServiceProvider::class,
-        ExcelServiceProvider::class,
-        AppServiceProvider::class,
-    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
