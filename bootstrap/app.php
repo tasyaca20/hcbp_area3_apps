3a6d6892cd4b5c6944592b91fd08babb20ca2ea6
tasyaca20 <tasyaaaputriii09@gmail.com>
2026-08-31 00:19:47 -0700

    fix: restore clean Laravel bootstrap provider registration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,35 +1,18 @@
 <?php
 
 use App\Providers\AppServiceProvider;
-use Illuminate\Cache\CacheServiceProvider;
-use Illuminate\Cookie\CookieServiceProvider;
-use Illuminate\Database\DatabaseServiceProvider;
-use Illuminate\Encryption\EncryptionServiceProvider;
-use Illuminate\Filesystem\FilesystemServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Hashing\HashServiceProvider;
 use Illuminate\Http\Request;
-use Illuminate\Pagination\PaginationServiceProvider;
-use Illuminate\Session\SessionServiceProvider;
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Translation\TranslationServiceProvider;
-use Illuminate\Validation\ValidationServiceProvider;
-use Illuminate\View\ViewServiceProvider;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(web: __DIR__.'/../routes/web.php', commands: __DIR__.'/../routes/console.php', health: '/up')
-    ->withProviders([
-        CacheServiceProvider::class, CookieServiceProvider::class, DatabaseServiceProvider::class,
-        EncryptionServiceProvider::class, FilesystemServiceProvider::class, HashServiceProvider::class,
-        PaginationServiceProvider::class, SessionServiceProvider::class, TranslationServiceProvider::class,
-        ValidationServiceProvider::class, ViewServiceProvider::class, AppServiceProvider::class,
-    ])
-    ->registered(function (Application $app): void {
-        $app->register(FilesystemServiceProvider::class, true);
-        $app->register(ViewServiceProvider::class, true);
-    })
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withProviders([AppServiceProvider::class])
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias(['role' => \App\Http\Middleware\CheckRole::class]);
     })
