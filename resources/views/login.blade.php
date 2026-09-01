f3631c2da6a73f0ddf12509767f8507665f2fbed
akbarzamzami <akbarzamzami07728@gmail.com>
2026-09-01 10:55:55 +0700

    fix: add favicon link to login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,6 +6,7 @@
   <meta content="width=device-width, initial-scale=1.0" name="viewport" />
   <title>HCBP Area 3 Apps - Login</title>
   <link href="https://fonts.googleapis.com" rel="preconnect" />
+  <link rel="icon" type="image/png" href="{{ asset('images/logo_pln.png') }}"/>
   <link crossorigin="" href="https://fonts.gstatic.com" rel="preconnect" />
   <link
     href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap"
